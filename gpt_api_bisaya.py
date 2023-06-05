213a618f6d1fd25422d63b533b605bb7fed88606
Seejee Genegabuas <gseejee@gmail.com>
2023-06-05 22:56:08 +0800

    last push

--- gpt_api_bisaya.py
+++ gpt_api_bisaya.py
@@ -11,11 +11,20 @@ def translate(text):
 
 def generate_prompt(text):
     return """Translate the mix of English and Bisaya words into English based on the context of the English text. The output should be English.
+    In cases where one of the words in the bisaya text does not make sense given the sentence structure, find the most suitable bisaya word and replace it and then translate to english.
+    In cases where the text is purely bisaya, translte it into english with proper grammar.
+    In cases where the text is purely english, just use the input as the output.
+    In cases where the text doesnt make sense, reply with (incomprehensible).
+    In cases where you require more context, reply with (incomprehensible).
 
     Input: I am very gwapo.
     Output: I am very handsome.
     Input: Despite sa kaguol, he picked himself up. Kaya ra lagi na, that's what he thought.
     Output: Despite the sorrow, he picked himself up. He thought he could do it, that's why.
+    Input: Dance usa then more three lighht.
+    Output: ...
+    Input: Naa nami danri.
+    Output: We're already here.
     Input: {}
     Output:""".format(
             text.capitalize()
--- gpt_api_tagalog.py
+++ gpt_api_tagalog.py
@@ -12,8 +12,12 @@ def translate(text):
     return response.choices[0].text
 
 def generate_prompt(text):
-    return """Translate the mix of English and Tagalog words into English based on the context of the English text. The output should be English. In cases where the text is purely tagalog, translate it into english with proper grammar. In cases where the text is purely english, just use the input as the output.
-
+    return """Translate the mix of English and Tagalog words into English based on the context of the English text. 
+    The output should be English. In cases where the text is purely tagalog, translate it into english with proper grammar. 
+    In cases where the text is purely english, just use the input as the output.
+    In cases where the input doesnt make sense, reply with ellipses.
+    In cases where you require more context, reply with ellipses.
+    
     Input: I am very gwapo.
     Output: I am very handsome.
     Input: Kahit pagod na pagod na siya, he picked himself up. Kaya ko ito, that's what he thought.
